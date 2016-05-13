170a1a9469b57c55fce79aecb81624841b663d17
mjoyner <mjoyner@vbservices.net>
2016-05-13 17:59:31 -0400

    initial simple ui implementation for text mode only output

---
+++ ExerciseInterleavedMaker/src/main/java/com/cherokeelessons/eim/App.java
@@ -0,0 +1,18 @@
+package com.cherokeelessons.eim;
+
+public class App implements Runnable {
+
+	public App(String[] args) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		_run();
+	}
+
+	private void _run() {
+		
+	}
+
+}
---
+++ ExerciseInterleavedMaker/src/main/java/com/cherokeelessons/eim/Main.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.cherokeelessons.eim;
+
+import java.awt.EventQueue;
+
+import com.newsrx.gui.MainWindow;
+
+/**
+ * @author mjoyner
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainWindow.Config config=new MainWindow.Config() {
+			@Override
+			public String getApptitle() {
+				return "Exercise Interleaved Maker";
+			}
+			
+			@Override
+			public Runnable getApp(String... args) throws Exception {
+				return new App(args);
+			}
+		};
+		EventQueue.invokeLater(new MainWindow(config, args));
+	}
+
+}
